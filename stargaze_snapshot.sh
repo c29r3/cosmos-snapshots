b981237a8a8b2efd54e66485a8e11cd0cedcdf56
Sergey <52304443+c29r3@users.noreply.github.com>
2022-01-24 22:17:57 +0300

    Create stargaze_snapshot.sh

---
+++ stargaze_snapshot.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+CHAIN_ID="stargaze-1"
+SNAP_PATH="$HOME/stargaze/stargaze"
+LOG_PATH="$HOME/stargaze/stargaze_log.txt"
+DATA_PATH="$HOME/.starsd/data/"
+SERVICE_NAME="stargaze.service"
+RPC_ADDRESS="http://135.181.60.250:16557"
+SNAP_NAME=$(echo "${CHAIN_ID}_$(date '+%Y-%m-%d').tar")
+OLD_SNAP=$(ls ${SNAP_PATH} | egrep -o "${CHAIN_ID}.*tar")
+
+
+now_date() {
+    echo -n $(TZ=":Europe/Moscow" date '+%Y-%m-%d_%H:%M:%S')
+}
+
+
+log_this() {
+    YEL='\033[1;33m' # yellow
+    NC='\033[0m'     # No Color
+    local logging="$@"
+    printf "|$(now_date)| $logging\n" | tee -a ${LOG_PATH}
+}
+
+LAST_BLOCK_HEIGHT=$(curl -s ${RPC_ADDRESS}/status | jq -r .result.sync_info.latest_block_height)
+log_this "LAST_BLOCK_HEIGHT ${LAST_BLOCK_HEIGHT}"
+
+log_this "Stopping ${SERVICE_NAME}"
+systemctl stop ${SERVICE_NAME}; echo $? >> ${LOG_PATH}
+
+log_this "Creating new snapshot"
+time tar cf ${HOME}/${SNAP_NAME} -C ${DATA_PATH} . &>>${LOG_PATH}
+
+log_this "Starting ${SERVICE_NAME}"
+systemctl start ${SERVICE_NAME}; echo $? >> ${LOG_PATH}
+
+log_this "Removing old snapshot(s):"
+cd ${SNAP_PATH}
+rm -fv ${OLD_SNAP} &>>${LOG_PATH}
+
+log_this "Moving new snapshot to ${SNAP_PATH}"
+mv ${HOME}/${CHAIN_ID}*tar ${SNAP_PATH} &>>${LOG_PATH}
+
+
+du -hs ${SNAP_PATH} | tee -a ${LOG_PATH}
+
+log_this "Done\n---------------------------\n"
